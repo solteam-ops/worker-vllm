a6cfda188897b87cdf9d06ee77c1a2122d4316e6
root <leejw@gabia.com>
2025-10-02 17:56:52 +0900

    feat: Initial commit with vLLM v0.10.1 support

---
+++ src/handler.py
@@ -0,0 +1,22 @@
+import os
+import runpod
+from utils import JobInput
+from engine import vLLMEngine, OpenAIvLLMEngine
+
+vllm_engine = vLLMEngine()
+OpenAIvLLMEngine = OpenAIvLLMEngine(vllm_engine)
+
+async def handler(job):
+    job_input = JobInput(job["input"])
+    engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
+    results_generator = engine.generate(job_input)
+    async for batch in results_generator:
+        yield batch
+
+runpod.serverless.start(
+    {
+        "handler": handler,
+        "concurrency_modifier": lambda x: vllm_engine.max_concurrency,
+        "return_aggregate_stream": True,
+    }
+)
